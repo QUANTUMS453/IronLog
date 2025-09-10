389354a69de7ec3d0a9a09f3d2657c5e7940291c
QUANTUMS453 <hossein543251@gmail.com>
2025-09-10 23:45:38 +0330

    Update analyzer.py with latest changes

--- IronLog/analyzer.py
+++ IronLog/analyzer.py
@@ -1,14 +1,22 @@
 import numpy as np
 import pandas as pd
 
+def session_summaries(df: pd.DataFrame):
+    df["date_label"] = df["date"].rank(method="dense").astype(int)
+    return df
 
 def basic_stats(df: pd.DataFrame):
     df["volume"] = df["sets"] * df["reps"] * df["weight"]
-    result = df.groupby("exercise")["volume"].sum().reset_index()
+    result = df.groupby("exercise").agg({
+        "volume": "sum",
+        "sets": "sum",
+        "weight": ["max", "mean"]
+        }).reset_index()
+    result.columns = ["exercise", "total_volume", "total_sets", "max_weight", "avg_weight"]
 
     return result
 
 def high_finder(df: pd.DataFrame):
     max_weight = df.groupby("exercise")["weight"].max()
-
     return max_weight
+    
