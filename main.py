ae8687132bc3974bafeacf6d6dc1037ea1b2548b
QUANTUMS453 <hossein543251@gmail.com>
2025-09-11 21:28:25 +0330

    adjustment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 from IronLog.loader import load_data, clean_data
 from IronLog.analyzer import summarize
+from IronLog.visualizer import data_visualizer
 
 def main():
     raw = load_data(r"D:\IronLog project\IronLog\Data\Workout_Data.csv")
@@ -15,6 +18,8 @@ def main():
     excercise_summary = summarize(df, by="exercise")
     print("[OK] Summarized data\n", excercise_summary)
 
+    plot1 = data_visualizer(excercise_summary)
+    plt.show()
 
 if __name__ == "__main__":
     main()
