0453f3cdea757b403c3b07fdeeb1d986f3fc523c
QUANTUMS453 <hossein543251@gmail.com>
2025-09-12 15:45:43 +0330

    edit functions and it now save plot image

--- IronLog/visualizer.py
+++ IronLog/visualizer.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+# Visualizes total volume and max weight per exercise.
 def data_visualizer(df: pd.DataFrame):
-    plt.bar(df["exercise"], df["total_volume"], color="skyblue")
-    plt.title("Total Volume per Exercise")
-    plt.xlabel("Exercise")
-    plt.figure()
-    plt.bar(df["exercise"], df["max_weight"], color="Red")
-    plt.title("Max Weight per Exercise")
-    plt.xlabel("Exercise")
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    axes[0].bar(df["exercise"], df["total_volume"], color="skyblue")
+    axes[0].set_title("Total Volume by Exercise")
+    axes[0].set_xlabel("Exercise")
+    axes[0].set_ylabel("Total Volume")
+    axes[0].tick_params(axis="x", rotation=30)
+    axes[1].bar(df["exercise"], df["max_weight"], color="salmon")
+    axes[1].set_title("Max Weight by Exercise")
+    axes[1].set_xlabel("Exercise")
+    axes[1].set_ylabel("Max Weight")
+    axes[1].tick_params(axis="x", rotation=30)
+    plt.show()
+    return fig
